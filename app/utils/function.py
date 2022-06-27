44ef835ac7b385e0b33122021aa1591b33842318
Shaurya <int-shaurya.agarwal@1mg.com>
2022-06-28 01:50:23 +0530

    docs: add docstring for `utils` functions

--- app/utils/function.py
+++ app/utils/function.py
@@ -2,6 +2,14 @@ from typing import List
 
 
 def as_func(func):
+    """
+    Convert a bounded method to a function object.
+
+    :param func: A bounded method.
+
+    :return: A function object.
+    """
+
     def wrapper(*args, **kwargs):
         return func(*args, **kwargs)
     wrapper.__name__ = func.__name__
--- app/utils/route.py
+++ app/utils/route.py
@@ -2,6 +2,13 @@ from typing import List
 
 
 def route(uri: str, methods: List[str]):
+    """
+    Decorator used to define a route path and methods for a handler
+
+    :param uri: The path of the route
+    :param methods: The methods of the route
+    """
+
     def decorator(func):
         def wrapper(*args, **kwargs):
             return func(*args, **kwargs)
